055cfa019adc09245324ba594774394dedd956a4
Sokke91 <jonas.angel.ja@gmail.com>
2023-04-26 20:56:14 +0200

    add ordersetController

---
+++ backend/controllers/ordersetController.go
@@ -0,0 +1,12 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+func GetOrderSet(c *gin.Context, id int) {
+}
+
+func CreateOrderset(c *gin.Context) {
+}
+
+func DeleteOrderset(c *gin.Context) {
+}
